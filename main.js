bf467ff6bf799e1e719cba3aa7d2386af020cbf7
Nguyễn Đạt <dattiennguyen150600@gmail.co>
2021-04-16 17:07:11 +0700

    new file

---
+++ main.js
@@ -0,0 +1,7 @@
+function openSearch() {
+    document.getElementById("btnSearch").style.display = "block";
+  }
+  
+  function closeSearch() {
+    document.getElementById("btnSearch").style.display = "none";
+  }
